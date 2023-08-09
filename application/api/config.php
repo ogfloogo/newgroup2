15bc62ced8e153410739f5ed89e1b4dbdf62fb16
h <472131863@qq.com>
2023-08-09 12:42:20 +0800

    1

---
+++ application/api/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//配置文件
+return [
+    'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
+    'default_return_type'    => 'json',
+    'image_url' => "https://api.maygroup.shop",
+    'invite_url' => "https://www.maygroup.shop",
+];
